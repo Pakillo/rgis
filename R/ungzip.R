7bf2d18170f0328f299dd5a0e84b449c9ce44268
Pakillo <f.rodriguez.sanc@gmail.com>
2016-03-31 16:09:57 +0200

    added ungzip function

---
+++ R/ungzip.R
@@ -0,0 +1,21 @@
+#' Decompress gzip file
+#'
+#' Automatically decompress a gzip file so that it can be read directly by any of the \code{raster} import functions (\code{raster}, \code{stack}, \code{brick}). The gzip file is decompressed to a temporary file which is then read (while the original gzip file is kept). This is useful to keep and use directly large, gzip-compressed GIS files. Decompressed files are automatically deleted when cleaning temporary files.
+#'
+#' @param filepath Path to the gzip file.
+#'
+#' @importFrom R.utils decompressFile
+#'
+#' @return Path to the decompressed file (which can then be read by raster functions).
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(raster)
+#' gisdata <- brick(ungzip("~/GISdata/climate.nc.gz"))
+#' }
+ungzip <- function(filepath){
+  R.utils::decompressFile(filepath, ext="gz", FUN=gzfile,
+                          remove = FALSE, temporary = TRUE,
+                          skip = FALSE, overwrite = TRUE)
+}
