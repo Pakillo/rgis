7d264348d8a7dd943e8c4b06f1c6c22513b01a4a
Pakillo <f.rodriguez.sanc@gmail.com>
2016-03-31 15:46:09 +0200

    initial commit

---
+++ R/rgis-package.R
@@ -0,0 +1,11 @@
+#' rgis package
+#'
+#' Functions to perform GIS operations in R.
+#'
+#' @name rgis-package
+#' @docType package
+#' @author Francisco Rodriguez-Sanchez
+#' @keywords package
+#' @aliases rgis
+#'
+NULL
